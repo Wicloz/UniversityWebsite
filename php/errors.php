d48cb337cd934a32dcdd531c0ce7670ca652bb93
Wilco de Boer <deboer.wilco@gmail.com>
2015-10-27 22:27:58 +0100

    Improve 404 page

--- php/errors.php
+++ php/errors.php
@@ -1,7 +1,7 @@
 <?php
 
 function err_404 () {
-	return '<div class="paragraph-center col-sm-12"><h2>Error 404: Page not found</h2><p>The requested object could not be found. If you entered the URL manually, please check for errors. In case of a broken link, please contact the developer.</p></div>';
+	return '<div class="paragraph-center col-sm-12"><h2>Error 404: Page not found</h2><p>Sorry, it seems you were trying to access a page that doesn\'t exist. Please check the spelling of the URL you were trying to access and try again.<br>If you are of the opinion the server is at fault, please contact the developer.</p><img src="media/404.png" width=100% alt="404"></div>';
 }
 
 function err_500 () {

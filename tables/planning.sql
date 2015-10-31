4f95b6538715f719bf71fd08291b01af0f859301
Wilco de Boer <deboer.wilco@gmail.com>
2015-10-31 12:36:22 +0100

    Update and populate planning table

--- tables/planning.sql
+++ tables/planning.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 31 okt 2015 om 11:48
+-- Gegenereerd op: 31 okt 2015 om 12:36
 -- Serverversie: 5.6.26
 -- PHP-versie: 5.6.12
 
@@ -30,10 +30,19 @@ CREATE TABLE IF NOT EXISTS `planning` (
   `id` int(11) NOT NULL,
   `parent_table` varchar(20) NOT NULL,
   `parent_id` int(11) NOT NULL,
-  `date` date NOT NULL,
+  `date_start` date NOT NULL,
+  `date_end` date NOT NULL,
   `duration` time NOT NULL,
   `goal` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `planning`
+--
+
+INSERT INTO `planning` (`id`, `parent_table`, `parent_id`, `date_start`, `date_end`, `duration`, `goal`) VALUES
+(1, 'subjects', 6, '2015-10-31', '2015-10-31', '01:00:00', 'Read:\r\n- CH6, to p153\r\n- CH8, p203 to p211\r\n- CH36, p846 to p849\r\n- CH48, p1132 to p1138'),
+(2, 'assignments', 5, '2015-10-31', '2015-11-01', '00:00:00', 'Work on the webpage to meet the requirements for the assignment');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -53,7 +62,7 @@ ALTER TABLE `planning`
 -- AUTO_INCREMENT voor een tabel `planning`
 --
 ALTER TABLE `planning`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

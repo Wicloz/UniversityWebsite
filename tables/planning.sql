29cea4d71e3daf075e2fc282f2ea077ae5d72ca6
Wilco de Boer <deboer.wilco@gmail.com>
2015-11-05 00:39:27 +0100

    Update planning table

--- tables/planning.sql
+++ tables/planning.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 04 nov 2015 om 14:49
+-- Gegenereerd op: 05 nov 2015 om 00:37
 -- Serverversie: 5.6.26
 -- PHP-versie: 5.6.12
 
@@ -46,12 +46,12 @@ CREATE TABLE IF NOT EXISTS `planning` (
 INSERT INTO `planning` (`id`, `parent_table`, `parent_id`, `date_start`, `date_end`, `subject`, `duration`, `goal`, `finished_on`, `done`) VALUES
 (2, 'assignments', 5, '2015-10-31', '2015-11-01', 'Studeren & Presenteren', '00:00:00', 'Work on the webpage to meet the requirements for the assignment', '2015-11-01 22:00:00', 1),
 (3, 'assignments', 5, '2015-11-01', '2015-11-01', 'Studeren & Presenteren', '00:00:00', 'Make entries easier to insert and edit, especcially for the planning.', '2015-11-01 22:00:00', 1),
-(4, 'subjects', 4, '2015-11-03', '2015-11-04', 'Basispracticum', '02:00:00', 'Voorbereiden practicum fotosynthese', '0000-00-00 00:00:00', 0),
+(4, 'subjects', 4, '2015-11-03', '2015-11-04', 'Basispracticum', '02:00:00', 'Voorbereiden practicum fotosynthese', '2015-11-05 00:37:01', 1),
 (5, 'subjects', 6, '2015-11-02', '2015-11-02', 'Celfysiologie', '00:00:15', 'Lees: Hs10.1; Hs11', '2015-11-02 22:47:30', 1),
-(6, 'subjects', 6, '2015-11-04', '2015-11-05', 'Celfysiologie', '00:15:00', 'Lees: Hs10.4; Hs11', '0000-00-00 00:00:00', 0),
+(6, 'subjects', 6, '2015-11-04', '2015-11-05', 'Celfysiologie', '00:15:00', 'Lees: Hs10.4; Hs11', '2015-11-04 17:18:19', 1),
 (7, 'subjects', 3, '2015-11-03', '2015-11-08', 'Fundamentele Informatica 1', '01:00:00', 'Maak opgavenblad talen', '0000-00-00 00:00:00', 0),
-(8, 'assignments', 3, '2015-11-04', '2015-11-04', 'Programmeermethoden', '02:00:00', 'Afmaken verslag Life', '0000-00-00 00:00:00', 0),
-(10, 'subjects', 2, '2015-11-04', '2015-11-08', 'Programmeermethoden', '00:30:00', 'Maken opgavenblad werkcollege', '0000-00-00 00:00:00', 0);
+(8, 'assignments', 3, '2015-11-04', '2015-11-04', 'Programmeermethoden', '02:00:00', 'Maken verslag Life', '2015-11-04 19:20:26', 1),
+(10, 'subjects', 2, '2015-11-04', '2015-11-08', 'Programmeermethoden', '00:45:00', 'Maak opgavenblad werkcollege', '0000-00-00 00:00:00', 0);
 
 --
 -- Indexen voor geëxporteerde tabellen

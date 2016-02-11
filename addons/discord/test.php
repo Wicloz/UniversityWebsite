c790caa044338d368b25d7df00ba0005ce768de9
Wilco de Boer <deboer.wilco@gmail.com>
2016-02-11 23:36:09 +0100

    Testing things

--- addons/discord/test.php
+++ addons/discord/test.php
@@ -1,4 +1,10 @@
 <?php
 require 'connection.php';
-echo('dit is een test')
+use Discord\Helpers\RoleColors;
+$color = new RoleColors();
+$guildId = '96989967485517824';
+$roleId = '144548843730501632';
+$roleColor = $color->darkRed;
+
+$discord->api('guild')->roles()->edit($guildId, $roleId, null, null, null, null);
 ?>

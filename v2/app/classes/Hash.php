5c840c75f39cc21077f9c9d1e2623c4f531a11eb
Wilco de Boer <deboer.wilco@gmail.com>
2016-03-28 16:32:21 +0200

    Use more secure password hashing and salt generation

--- v2/app/classes/Hash.php
+++ v2/app/classes/Hash.php
@@ -1,15 +1,22 @@
 <?php
 class Hash {
-    public static function createHash ($string, $salt = '') {
+    public static function hashString ($string, $salt = '') {
         return hash('sha256', $string . $salt);
     }
 
-    public static function generateSalt ($length) {
-        return mcrypt_create_iv($length);
+    public static function generateSalt () {
+        $salt = mcrypt_create_iv(22, MCRYPT_DEV_URANDOM);
+        $salt = base64_encode($salt);
+        $salt = str_replace('+', '.', $salt);
+        return $salt;
+    }
+
+    public static function hashPassword ($password, $salt) {
+        return crypt($password, '$2y$10$'.$salt.'$');
     }
 
     public static function createUnique () {
-        return self::make(uniqid());
+        return self::hashString(uniqid());
     }
 }
 ?>

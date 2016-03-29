a9be5efc695cf028115a3045307184a38eec6348
Wilco de Boer <deboer.wilco@gmail.com>
2016-03-29 18:43:35 +0200

    Update DateFormat

--- v2/app/classes/DateFormat.php
+++ v2/app/classes/DateFormat.php
@@ -1,8 +1,8 @@
 <?php
 class DateFormat {
-    public static function sql ($time = '') {
-        if ($time === '') {
-            $time = time();
+    public static function sql ($timestamp = '') {
+        if ($timestamp === '') {
+            $timestamp = time();
         }
         return date('Y-m-d H:i:s', $time);
     }
--- v2/app/pages/login.php
+++ v2/app/pages/login.php
@@ -20,7 +20,7 @@ function createPage ($smarty) {
                         'email' => Input::get('email'),
                         'umail' => Input::get('sid').'@umail.leidenuniv.nl',
                         'phone' => Input::get('phone'),
-                        'joined' => DateFormat::sql();
+                        'joined' => DateFormat::sql()
                     ));
 
                     Users::login(Input::get('sid'), Input::get('password'));

6953858ce2ed3a33ce654abf6a8f55b84e9b5bcb
Wilco de Boer <deboer.wilco@gmail.com>
2016-03-29 17:07:32 +0200

    Update permissions table

--- tables/permissions.sql
+++ tables/permissions.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 28 mrt 2016 om 02:31
+-- Gegenereerd op: 29 mrt 2016 om 17:06
 -- Serverversie: 5.6.26
 -- PHP-versie: 5.6.12
 
@@ -23,31 +23,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `groups`
+-- Tabelstructuur voor tabel `permissions`
 --
 
-CREATE TABLE IF NOT EXISTS `groups` (
+CREATE TABLE IF NOT EXISTS `permissions` (
   `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `permissions` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
--- Gegevens worden geëxporteerd voor tabel `groups`
+-- Gegevens worden geëxporteerd voor tabel `permissions`
 --
 
-INSERT INTO `groups` (`id`, `name`, `permissions`) VALUES
+INSERT INTO `permissions` (`id`, `name`, `permissions`) VALUES
 (1, 'user', ''),
-(2, 'admin', '{"admin": 1}');
+(2, 'editor', '{"editor": 1}'),
+(3, 'admin', '{"editor": 1, "admin": 1}');
 
 --
 -- Indexen voor geëxporteerde tabellen
 --
 
 --
--- Indexen voor tabel `groups`
+-- Indexen voor tabel `permissions`
 --
-ALTER TABLE `groups`
+ALTER TABLE `permissions`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -55,10 +56,10 @@ ALTER TABLE `groups`
 --
 
 --
--- AUTO_INCREMENT voor een tabel `groups`
+-- AUTO_INCREMENT voor een tabel `permissions`
 --
-ALTER TABLE `groups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+ALTER TABLE `permissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

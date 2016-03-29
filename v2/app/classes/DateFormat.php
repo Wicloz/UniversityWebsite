595049e615f7042d0c921d75622bd1c42393ab68
Wilco de Boer <deboer.wilco@gmail.com>
2016-03-29 18:44:03 +0200

    Update DateFormat

--- v2/app/classes/DateFormat.php
+++ v2/app/classes/DateFormat.php
@@ -4,7 +4,7 @@ class DateFormat {
         if ($timestamp === '') {
             $timestamp = time();
         }
-        return date('Y-m-d H:i:s', $time);
+        return date('Y-m-d H:i:s', $timestamp);
     }
 }
 ?>

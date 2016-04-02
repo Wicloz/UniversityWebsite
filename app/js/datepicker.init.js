da0403c7368e1984c3b19c5078c09f8572bc6e9c
Wilco de Boer <deboer.wilco@gmail.com>
2016-04-02 21:50:38 +0200

    Normalize datetime format

--- app/js/datepicker.init.js
+++ app/js/datepicker.init.js
@@ -5,7 +5,7 @@ $(function () {
             vertical: 'bottom'
         },
         sideBySide: true,
-        format: 'YYYY/MM/DD, HH:mm'
+        format: 'YYYY-MM-DD, HH:mm'
     });
 });
 $(function () {
@@ -14,7 +14,7 @@ $(function () {
             horizontal: 'auto',
             vertical: 'bottom'
         },
-        format: 'YYYY/MM/DD'
+        format: 'YYYY-MM-DD'
     });
 });
 $(function () {

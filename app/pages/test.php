0e50defac78f3b98278c07695211ae44ec8370ba
Wilco de Boer <deboer.wilco@gmail.com>
2016-04-13 20:49:45 +0200

    Testing intensifies

--- app/pages/test.php
+++ app/pages/test.php
@@ -2,8 +2,17 @@
 $allowCaching = false;
 function createPage ($smarty) {
     if (exec('echo TEST') == 'TEST') {
-        echo 'exec works!';
+        echo 'Exec Works!<br>';
     }
+
+    if (file_exists('app/classes/anime/test.py')) {
+        echo 'File Exists!<br>';
+        echo exec('python app/classes/anime/test.py');
+        $command = escapeshellcmd('app/classes/anime/test.py');
+        $output = shell_exec($command);
+        echo $output;
+    }
+
     return $smarty;
 }
 ?>

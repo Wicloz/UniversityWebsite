7a272ea7200c6db69420ea329fea94acf486c235
Wilco de Boer <wfh@wilcodeboer.me>
2016-09-07 15:01:10 +0200

    Remove addons

--- addons/discord/db/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'dbsettings.php';
-
-$db = new mysqli($db_ip, $db_username, $db_password, $db_dbname);
-if ($db->connect_errno) {
-	die('Database errors, sorry for the inconvenience.');
-}
-?>

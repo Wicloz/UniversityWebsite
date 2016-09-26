4694d2595a506b6d7135f4350134a1a30d20d4ff
Wilco de Boer <wfh@wilcodeboer.me>
2016-09-26 14:14:25 +0200

    Remove anime stuff

--- app/classes/Anime.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'anime/Connector.KissAnime.php';
-
-?>
--- app/classes/anime/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-def hello():
-    print "Hi :)"
-
-if __name__ == "__main__":
-    hello()
-    return "test"

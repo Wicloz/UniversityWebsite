4cedfd1921e06628d94c7a3debb70c6d3313e3bf
Wilco de Boer <wfh@wilcodeboer.me>
2017-11-17 00:55:52 +0100

    Update datepicker javascript to prefer native solution

--- app/js/datepicker.init.js
+++ app/js/datepicker.init.js
@@ -1,59 +1,82 @@
 function initDatetime () {
+  var datetime = document.createElement("input");
+  datetime.setAttribute("type", "datetime");
+
+  if (datetime.type != "datetime") {
     $(function () {
-        $('.datetime').datetimepicker({
-            widgetPositioning: {
-                horizontal: 'auto',
-                vertical: 'bottom'
-            },
-            sideBySide: true,
-            format: 'DD-MM-YYYY, HH:mm'
-        });
+      $('.datetime').datetimepicker({
+        widgetPositioning: {
+          horizontal: 'auto',
+          vertical: 'bottom'
+        },
+        sideBySide: true,
+        format: 'DD-MM-YYYY, HH:mm'
+      });
     });
     $(function () {
-        $('.date').datetimepicker({
-            widgetPositioning: {
-                horizontal: 'auto',
-                vertical: 'bottom'
-            },
-            format: 'DD-MM-YYYY'
+      $('.datetimesql').datetimepicker({
+        widgetPositioning: {
+          horizontal: 'auto',
+          vertical: 'bottom'
+        },
+        sideBySide: true,
+        format: 'YYYY-MM-DD HH:mm:ss'
         });
+    });
+  }
+
+  datetime.parent().removeChild(datetime);
+
+  var date = document.createElement("input");
+  date.setAttribute("type", "date");
+
+  if (date.type != "date") {
+    $(function () {
+      $('.date').datetimepicker({
+        widgetPositioning: {
+          horizontal: 'auto',
+          vertical: 'bottom'
+        },
+        format: 'DD-MM-YYYY'
+      });
     });
     $(function () {
-        $('.time').datetimepicker({
-            widgetPositioning: {
-                horizontal: 'auto',
-                vertical: 'bottom'
-            },
-            format: 'HH:mm:ss'
-        });
+      $('.datesql').datetimepicker({
+        widgetPositioning: {
+          horizontal: 'auto',
+          vertical: 'bottom'
+        },
+        format: 'YYYY-MM-DD'
+      });
+    });
+  }
+
+  date.parent().removeChild(date);
+
+  var time = document.createElement("input");
+  time.setAttribute("type", "time");
+
+  if (time.type != "time") {
+    $(function () {
+      $('.time').datetimepicker({
+        widgetPositioning: {
+          horizontal: 'auto',
+          vertical: 'bottom'
+        },
+        format: 'HH:mm:ss'
+      });
     });
     $(function () {
-        $('.duration').datetimepicker({
-            widgetPositioning: {
-                horizontal: 'auto',
-                vertical: 'bottom'
-            },
-            useCurrent: false,
-            format: 'HH:mm:ss'
-        });
+      $('.duration').datetimepicker({
+        widgetPositioning: {
+          horizontal: 'auto',
+          vertical: 'bottom'
+        },
+        defaultDate: 0,
+        format: 'HH:mm:ss'
+      });
     });
-    $(function () {
-        $('.datesql').datetimepicker({
-            widgetPositioning: {
-                horizontal: 'auto',
-                vertical: 'bottom'
-            },
-            format: 'YYYY-MM-DD'
-        });
-    });
-    $(function () {
-        $('.datetimesql').datetimepicker({
-            widgetPositioning: {
-                horizontal: 'auto',
-                vertical: 'bottom'
-            },
-            sideBySide: true,
-            format: 'YYYY-MM-DD HH:mm:ss'
-        });
-    });
+  }
+
+  time.parent().removeChild(time);
 }

6b7a98d5c2176952f3704ce155881c0f15f93d24
Wilco de Boer <wfh@wilcodeboer.me>
2017-11-17 01:05:43 +0100

    Revert "Revert "Try to fix mobile""
    This reverts commit 82ca3b65201a3703c146c71fb29ba6537135b87d.

--- app/js/datepicker.init.js
+++ app/js/datepicker.init.js
@@ -2,7 +2,7 @@ function initDatetime () {
   var datetime = document.createElement("input");
   datetime.setAttribute("type", "datetime");
 
-  if (datetime.type != "datetime") {
+  if (datetime.type === "text") {
     $(function () {
       $('.datetime').datetimepicker({
         widgetPositioning: {
@@ -31,7 +31,7 @@ function initDatetime () {
   var date = document.createElement("input");
   date.setAttribute("type", "date");
 
-  if (date.type != "date") {
+  if (date.type === "text") {
     $(function () {
       $('.date').datetimepicker({
         widgetPositioning: {
@@ -58,7 +58,7 @@ function initDatetime () {
   var time = document.createElement("input");
   time.setAttribute("type", "time");
 
-  if (time.type != "time") {
+  if (time.type === "text") {
     $(function () {
       $('.time').datetimepicker({
         widgetPositioning: {
